c5c8936b7b93671cad5ee481539e7f97b80153cc
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-10-12 03:05:14 +0530

    Update ProjectCard.jsx

--- src/cards/projectCard/ProjectCard.jsx
+++ src/cards/projectCard/ProjectCard.jsx
@@ -1,46 +1,45 @@
 import "./projectCard.css";
  
 const ProjectCard = ({
-  image,
-  description,
   liveSite,
   github,
+  description,
   title,
   children,
-  design,
-  hiddenClass,
+  ytlink
 }) => {
   return (
     <div className="project-card-container">
-      <div
-        className="project_image_cont"
-        style={{
-          background: `url(${image})`,
-        }}
-      ></div>
-
-      <p className="project--title">{title}</p>
-
-      <p className="project-desc">{description}</p>
- 
-      <div className="flex stacks_description_cont">
-        STACKS USED:
-        {children}
-      </div>
-
-      <div className="flex project-links-cont">
-        <a
-          href={github}
-          target="blank"
-          aria-label="view code base on github"
-          className={`flex links__cont ${hiddenClass}`}
-        >
-          <span className="project-links">
-            <i className="fa-brands fa-github"></i>
-          </span>
-          <p className="btn link_text">GitHub</p>
-        </a>
-        {title !== "PORTFOLIO WEBSITE" ? (
+      <iframe src="https://player.vimeo.com/video/856648267?h=a42e1ba231&title=0&byline=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+      <div>
+        <p className="project--title">{title}</p>
+        <br />
+        <p className="project-desc">
+          <ul>
+            {description.map((item, index) => (
+              <div key={index}>
+                <li className="inside-des-project">{item.heading}</li>
+                <p>{item.brief}</p>
+                <br />
+              </div>
+            ))}
+          </ul>
+        </p>
+        <div className="flex stacks_description_cont">
+          STACKS USED: {children}
+        </div>
+        <div className="flex project-links-cont">
+          <a
+            href={github}
+            target="blank"
+            aria-label="view code base on github"
+            className={`flex links__cont`}
+          >
+            <span className="project-links">
+              <i className="fa-brands fa-github"></i>
+            </span>
+            <p className="btn link_text">GitHub</p>
+          </a>
           <a
             href={liveSite}
             target="blank"
@@ -52,9 +51,9 @@ const ProjectCard = ({
             </span>
             <p className="btn link_text">Live Link</p>
           </a>
-        ) : (
-          <p className="btn link_text">This Page</p>
-        )}
+        </div>
+        <br />
+        <br />
       </div>
     </div>
   );
