435a50955a8ca69b1a2e3b5ac015224aa19c2ba8
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-10-23 12:11:19 +0530

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,10 @@ import Hero from "../components/hero/Hero";
 import Project from "../components/projects/Project";
 import Skills from "../components/skills/Skills";
 import Education from "../components/Education/education"
-
+import { useEffect, useState } from "react";
+import { AnimatePresence , motion} from "framer-motion";
+import Preloader from "../components/Preloader/index.jsx";
+import NavBar from "../components/navbar/NavBar.jsx"
 const Main = () => {
   const contactClick = useRef("#contact");
 
@@ -13,8 +16,47 @@ const Main = () => {
     contactClick.current.scrollIntoView({ behavior: "smooth" });
   };
 
+  const leftAboutVariant = {
+    hidden: {
+      x: "-100%",
+      opacity: 0,
+      scale: 0,
+    },
+    show: {
+      x: 0,
+      opacity: 1,
+      rotate: 0,
+      scale: 1,
+      transition: {
+        type: "linear",
+        bounce: 0,
+        duration: 1,
+      },
+    },
+  };
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+
   return (
     <div>
+      <AnimatePresence mode="wait">
+          {isLoading && <Preloader />}
+      </AnimatePresence>
+      
+      {!isLoading && <NavBar/>}
       <Hero onClick={handleClick} />
       <About />
       <Project />
