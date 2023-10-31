e710085c930cade1396293d7eca535eca9216f9f
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-11-01 04:22:38 +0530

    Update projectObject.js

--- src/Object Files/projectObject.js
+++ src/Object Files/projectObject.js
@@ -3,8 +3,8 @@ import { v4 as uuid } from "uuid";
 export const projectData = [
   {
     id: uuid(),
-    ytlink: "https://www.youtube.com/embed/FxAG_11PzCk?si=p_8D5eQj3e4FHnko?rel=0",
-    title: "VibeVerse (Social Media Platform)",
+    ytlink: "https://www.youtube-nocookie.com/embed/5A9cxIOi5yc?si=XBXUP_lwGzM9c9Dv",
+    title: "VibeVerse (Social Media Platform) - Full Stack Project",
     description: [{heading: "JWT Authentication" , brief: "To ensure the utmost security, Vibevers utilizes JSON Web Tokens (JWT) for authentication. Every visit to the platform is authenticated, guaranteeing that only authorized users gain access to their accounts and data."} , 
                   {heading: "Password Encryption with Bcrypt" , brief: "User passwords are securely stored using Bcrypt encryption, enhancing data security and confidentiality. This industry-standard encryption technique safeguards user credentials from potential threats."} ,
                   {heading: "React for Intuitive Frontend" , brief: "Vibeverse boasts an intuitive frontend developed with React. The user interface is designed with a user-centric approach, providing a visually appealing and user-friendly environment."} ,
@@ -21,7 +21,7 @@ export const projectData = [
   {
     id: uuid(),
     ytlink: "https://www.youtube.com/embed/FxAG_11PzCk?si=p_8D5eQj3e4FHnko?rel=0",
-    title: "Marketplace (Online Shopping Platform)",
+    title: "Marketplace (Online Shopping Platform) - Full Stack Project",
     description: [{heading: "Backend Management using Strapi" , brief: "Used Strapi, a powerful content management system, to build a robust backend. This enabled me to easily add, update, and manage product listings with images, descriptions, and pricing."} , 
                   {heading: "Secure Payment Integration using Stripe" , brief: "Integrated Stripe, a trusted payment gateway, to ensure secure and efficient payment processing. Users can confidently add items to their cart and complete transactions with the assurance of a safe payment platform."} , 
                   {heading: "React" , brief : "Developed the frontend using React, a dynamic JavaScript library. This choice allowed for a highly responsive and user-friendly shopping experience, adapting seamlessly to various devices and screen sizes."}  ,
@@ -29,7 +29,7 @@ export const projectData = [
                   {heading: "Simplified API Calls" , brief : "Utilized 'createAsyncThunk' from Redux Toolkit in combination with Axios to make API calls to the backend. This approach streamlined communication between the frontend and backend, resulting in a more user-friendly and efficient experience."}  , 
                   {heading: "Hosting" , brief : "Hosted the backend and database on Digital Ocean, a reputable cloud infrastructure provider. This ensures scalability, performance, and reliability, essential for the seamless operation of the application."}  
                 ],
-    stacks: ["React", "CSS", "API" , "Strapi" , "Redux" , "SASS" , "Stripe" , "Axios"],
+    stacks: ["React" , "API" , "Strapi" , "Redux" , "SASS" , "Stripe" , "Axios"],
     liveSite: "",
     github : ""
   },
@@ -40,9 +40,9 @@ export const projectData = [
     description: [{heading: "React" , brief: "At the core of my portfolio site is React, a dynamic JavaScript library renowned for its ability to create rich, interactive user interfaces. React's component-based architecture empowers me to craft a responsive and aesthetically pleasing site that adapts seamlessly to different devices and screen sizes."} , 
                   {heading: "Framer Motion" , brief: "To captivate visitors, I harnessed the capabilities of Framer Motion, an animation library for React. This tool breathes life into my site, creating a visually engaging and memorable user experience. The subtle transitions, smooth scroll effects, and eye-catching animations not only showcase my dedication to aesthetic detail but also keep visitors engaged."},
                   {heading: "EmailJS" , brief: "One of the features of my portfolio site is the ability to send emails directly from the client side without the need for server-side processing. I achieved this using the emailjs/browser library, which simplifies the process of connecting with my audience, potential clients, or collaborators."} , 
-                  {heading: "My Portfolio Snapshot" , brief: "My personal portfolio website elegantly showcases my professional journey. It features a diverse collection of projects, a comprehensive breakdown of my skills, and insights into my educational background, offering visitors a glimpse into my expertise and creative capabilities."}
+                  {heading: "My Portfolio Snapshot" , brief: "My personal portfolio website elegantly showcases my journey. It features a diverse collection of projects, a comprehensive breakdown of my skills, and insights into my educational background, offering visitors a glimpse into my expertise and creative capabilities."}
                   ],
-    stacks: ["React", "CSS", "JSX"],
+    stacks: ["React", "CSS", "JSX" , "Framer-motion"],
     liveSite: "https://yadavdipesh.live/",
     github: "https://github.com/dipeshyadav331/Portfolio-website"
   },
