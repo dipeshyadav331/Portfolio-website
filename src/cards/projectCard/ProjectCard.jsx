44346d04e5e6bf6c8eba55f69f981d2956cf9b77
Dipesh Yadav <122032063+dipeshyadav331@users.noreply.github.com>
2023-11-01 04:35:58 +0530

    Add files via upload

---
+++ src/cards/projectCard/ProjectCard.jsx
@@ -0,0 +1,63 @@
+import "./projectCard.css";
+ 
+const ProjectCard = ({
+  liveSite,
+  github,
+  description,
+  title,
+  children,
+  ytlink
+}) => {
+  return (
+    <div className="project-card-container">
+        <iframe width="560" height="315" src={ytlink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <div>
+        <p className="project--title">{title}</p>
+        <br />
+        <p className="project-desc">
+          <ul>
+            {description.map((item, index) => (
+              <div key={index}>
+                <li className="inside-des-project">{item.heading}</li>
+                <p>{item.brief}</p>
+                <br />
+              </div>
+            ))}
+          </ul>
+        </p>
+        <div className="flex stacks_description_cont">
+          <span style={{ color: 'white' }}>STACKS USED: </span> {children}
+        </div>
+
+        <div className="flex project-links-cont">
+          <a
+            href={github}
+            target="blank"
+            aria-label="view code base on github"
+            className={`flex links__cont`}
+          >
+            <span className="project-links">
+              <i className="fa-brands fa-github"></i>
+            </span>
+            <p className="btn link_text">GitHub</p>
+          </a>
+          <a
+            href={liveSite}
+            target="blank"
+            aria-label="view live site"
+            className="flex links__cont"
+          >
+            <span className="project-links">
+              <i className="fa-solid fa-link"></i>
+            </span>
+            <p className="btn link_text">Live Link</p>
+          </a>
+        </div>
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
