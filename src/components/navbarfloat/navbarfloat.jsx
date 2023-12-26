8db52da968035c675edadf618f95a2d991277682
Dipesh <dipesh.kumar.ug21@nsut.ac.in>
2023-12-27 01:03:52 +0530

    added Floating avbar

--- src/components/navbarfloat/navbarfloat.jsx
+++ src/components/navbarfloat/navbarfloat.jsx
@@ -1,14 +1,13 @@
-import { useState , useEffect } from "react";
-import "./navbarfloat.css";
+import { useState, useEffect } from "react";
+import "./navbarfloat.scss";
 import navData from "../../Object Files/nav";
 
-import { motion , useAnimate, stagger } from "framer-motion";
+import { motion, useAnimate, stagger } from "framer-motion";
 
 const Navbarfloat = () => {
-
   const [open, setOpen] = useState(false);
   const [scope, animate] = useAnimate();
- 
+
   const staggerList = stagger(0.1, { startDelay: 0.25 });
 
   useEffect(() => {
@@ -38,26 +37,63 @@ const Navbarfloat = () => {
   }, [open]);
 
   return (
-    <div className="navbar_right" ref={scope}>
-      <motion.button
-        onClick={() => setOpen(!open)}
-        whileTap={{ scale: 0.95 }}
-        className="app_button"
-      >
-        Jump To
-      </motion.button>
-      <ul className="app_ul">
-        {navData.map((item, index) => (
-          <motion.li
-            key={index}
-            className="app_li"
-            onClick={() => setOpen(false)}
-          >
-            <a href={`${item.link}`}>{item.description}</a>
-          </motion.li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="navbar_right" ref={scope}>
+      <div id="hamburger" onClick={() => setOpen(!open)} className={`hamburglar ${open ? 'is-open' : 'is-closed'}`}>
+        <div className="burger-icon">
+          <div className="burger-container">
+            <span className="burger-bun-top"></span>
+            <span className="burger-filling"></span>
+            <span className="burger-bun-bot"></span>
+          </div>
+        </div>
+
+        {/* svg ring container */}
+        <div className="burger-ring">
+          <svg className="svg-ring">
+            <path
+              className="path"
+              fill="none"
+              stroke="#fff"
+              strokeMiterlimit="10"
+              strokeWidth="4"
+              d="M 34 2 C 16.3 2 2 16.3 2 34 s 14.3 32 32 32 s 32 -14.3 32 -32 S 51.7 2 34 2"
+            />
+          </svg>
+        </div>
+        {/* the masked path that animates the fill to the ring */}
+        <svg width="0" height="0">
+          <mask id="mask">
+            <path
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="#ff0000"
+              strokeMiterlimit="10"
+              strokeWidth="4"
+              d="M 34 2 c 11.6 0 21.8 6.2 27.4 15.5 c 2.9 4.8 5 16.5 -9.4 16.5 h -4"
+            />
+          </mask>
+        </svg>
+        <div className="path-burger">
+          <div className="animate-path">
+            <div className="path-rotation"></div>
+          </div>
+        </div>
+      </div>
+        
+        <ul className="app_ul">
+          {navData.map((item, index) => (
+            <motion.li
+              key={index}
+              className="app_li"
+              onClick={() => setOpen(false)}
+            >
+              <a href={`${item.link}`}>{item.description}</a>
+            </motion.li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
